2fb0fd768b6050858e29ba2b869f4acef4143d18
Aildson Ferreira <aildson.sh@gmail.com>
2023-12-03 17:58:13 -0300

    configuração inicial do nextjs utilizando page router

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+// TODO: Colocar todos os imports dos futuros componentes, para centralizar os imports em /components
+
+// Cada componente deverá ter sua própria pasta, contendo os seguintes arquivos: <nome_componente>.js, <nome_componente>.css e index.js
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>EcoTur - Frontend</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
+
+      <main>
+        <h1>Home</h1>
+      </main>
+    </>
+  );
+}
