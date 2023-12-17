5c7c935288bc6ac63711e521d19de616e3020c07
Eduardo Diniz <eduardogomesdiniz42@gmail.com>
2023-12-17 20:49:40 -0300

    Create places mocked data

---
+++ src/mockes/places.js
@@ -0,0 +1,99 @@
+const data = [
+    {
+      id: 1,
+      imageUrl: 'https://picsum.photos/400/200',
+      title: 'Cachoeira Paraíso',
+      subtitle: 'Lugar',
+      startDate: '2 Agosto',
+      endDate: '4 Agosto',
+      difficulty: 'Difícil',
+      price: 'R$ 70 por pessoa',
+      link:'www.google.com'
+    },
+    {
+      id: 2,
+      imageUrl: 'https://picsum.photos/400/200',
+      title: 'Montanha Aventura',
+      subtitle: 'Trilha',
+      startDate: '5 Agosto',
+      endDate: '7 Agosto',
+      difficulty: 'Moderado',
+      price: 'R$ 50 por pessoa',
+      link:'www.google.com'
+  
+    },
+    {
+      id: 3,
+      imageUrl: 'https://picsum.photos/400/200',
+      title: 'Praia Relaxante',
+      subtitle: 'Lugar',
+      startDate: '10 Agosto',
+      endDate: '12 Agosto',
+      difficulty: 'Fácil',
+      price: 'R$ 40 por pessoa',
+      link:'www.google.com'
+  
+    },
+    {
+      id: 4,
+      imageUrl: 'https://picsum.photos/400/200',
+      title: 'Aventura na Floresta',
+      subtitle: 'Trilha',
+      startDate: '15 Agosto',
+      endDate: '18 Agosto',
+      difficulty: 'Difícil',
+      price: 'R$ 80 por pessoa',
+      link:'www.google.com'
+  
+    },
+    {
+      id: 5,
+      imageUrl: 'https://picsum.photos/400/200',
+      title: 'Exploração Urbana',
+      subtitle: 'Cidade',
+      startDate: '20 Agosto',
+      endDate: '22 Agosto',
+      difficulty: 'Moderado',
+      price: 'R$ 60 por pessoa',
+      link:'www.google.com'
+  
+    },
+    {
+      id: 6,
+      imageUrl: 'https://picsum.photos/400/200',
+      title: 'Retiro nas Montanhas',
+      subtitle: 'Trilha',
+      startDate: '25 Agosto',
+      endDate: '28 Agosto',
+      difficulty: 'Fácil',
+      price: 'R$ 45 por pessoa',
+      link:'www.google.com'
+  
+    },
+    {
+      id: 7,
+      imageUrl: 'https://picsum.photos/400/200',
+      title: 'Passeio de Barco',
+      subtitle: 'Lugar',
+      startDate: '1 Setembro',
+      endDate: '3 Setembro',
+      difficulty: 'Fácil',
+      price: 'R$ 55 por pessoa',
+      link:'www.google.com'
+  
+    },
+    {
+      id: 8,
+      imageUrl: 'https://picsum.photos/400/200',
+      title: 'Desafio na Montanha',
+      subtitle: 'Trilha',
+      startDate: '7 Setembro',
+      endDate: '10 Setembro',
+      difficulty: 'Difícil',
+      price: 'R$ 75 por pessoa',
+      link:'www.google.com'
+  
+    },
+  ];
+
+  export default data;
